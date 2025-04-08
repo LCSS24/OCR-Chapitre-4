447aed40e723c75b0f967bc9f75d0c5912bbc621
Lucas oui <lucas.carpentier24@gmail.com>
2025-04-08 10:22:03 +0200

    Responsive terminé

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
+import './styles/index.scss'
 // import AppRouter from './utils/Router.jsx'
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
